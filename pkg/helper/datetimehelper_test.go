7256921000699772e3cd6321e7b8422d64df7aab
Muhammed Esad KAYA <muhammedesadkaya@gmail.com>
2022-04-13 20:54:19 +0300

    fix helper test v2

--- pkg/helper/datetimehelper_test.go
+++ pkg/helper/datetimehelper_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func Test_CheckAgeAdult(t *testing.T) {
+func Test_CheckAgeAdult_AdultExcepted(t *testing.T) {
 	//Arrange
 	testTime := time.Date(1993, time.Month(8), 26, 1, 10, 30, 0, time.UTC)
 
